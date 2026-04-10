41187deae625ac45e7a243427b1723c76e4d7bc8
Deepthi <maddudeepthi02@gmail.com>
2026-04-10 11:46:59 +0530

    add API testing

--- test/test.js
+++ test/test.js
@@ -1,9 +1,25 @@
-const assert = require('assert');
+const http = require('http');
 
-function add(a, b) {
-  return a + b;
+function testAPI() {
+  return new Promise((resolve, reject) => {
+    http.get('http://localhost:3000/', (res) => {
+      if (res.statusCode === 200) {
+        resolve();
+      } else {
+        reject("API failed");
+      }
+    }).on('error', reject);
+  });
 }
 
-assert.equal(add(2, 3), 5);
+async function runTests() {
+  try {
+    await testAPI();
+    console.log("API test passed!");
+  } catch (err) {
+    console.error("Test failed:", err);
+    process.exit(1);
+  }
+}
 
-console.log("All tests passed!");
+runTests();
